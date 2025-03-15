80b590f10d2d51d8f714eb037880c7d0b6d4f68a
percyzavala <percyzavala@gmail.com>
2025-03-14 23:25:23 -0500

    InfinitScroll con imagenes

--- src/presentation/screens/ui/InfiniteScrollScreen.tsx
+++ src/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, Image, ActivityIndicator} from 'react-native';
 import {CustomView} from '../../components/ui/CustomView';
 import {Title} from '../../components/ui/Title';
 import {colors} from '../../../config/theme/theme';
@@ -21,34 +21,35 @@ export const InfiniteScrollScreen = () => {
     const newArray = Array.from({length: 5}, (_, i) =>
       (numbers.length + i + 1).toString(),
     );
-    setTimeout(() => {
-      console.log('loading');
-      setNumbers([...numbers, ...newArray]);
-    }, 3000);
+    setNumbers([...numbers, ...newArray]);
   };
   // render
   return (
-    <CustomView margin>
-      <Title title="Infinite Scroll" safe />
+    <View style={{backgroundColor: 'black'}}>
       <FlatList
         data={numbers}
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
         keyExtractor={item => item}
-        renderItem={({item}) => (
-          <Text
-            style={{
-              height: 300,
-              backgroundColor: colors.primary,
-              color: 'white',
-              fontSize: 50,
-              textAlign: 'center',
-            }}>
-            {' '}
-            {item}{' '}
-          </Text>
+        renderItem={({item}) => <ListItem number={parseInt(item)} />}
+        ListFooterComponent={() => (
+          <View style={{height: 150, justifyContent: 'center'}}>
+            
+            <ActivityIndicator size={40} color={colors.primary} />
+          </View>
         )}
       />
-    </CustomView>
+    </View>
+  );
+};
+type ListItemProps = {
+  number: number;
+};
+export const ListItem = ({number}: ListItemProps) => {
+  return (
+    <Image
+      source={{uri: `https://picsum.photos/id/${number}/200/300`}}
+      style={{height: 400, width: '100%'}}
+    />
   );
 };
