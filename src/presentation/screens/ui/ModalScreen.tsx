c380ee3c48da5573324573063fafc4078648f4a7
percyzavala <percyzavala@gmail.com>
2025-03-14 22:04:00 -0500

    se agrego el companente Modal

--- src/presentation/screens/ui/ModalScreen.tsx
+++ src/presentation/screens/ui/ModalScreen.tsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react'
-import { View, Text, Modal } from 'react-native'
-import { CustomView } from '../../components/ui/CustomView';
-import { Title } from '../../components/ui/Title';
-import { Button } from '../../components/ui/Button';
+import React, {useState} from 'react';
+import {View, Text, Modal, ViewBase} from 'react-native';
+import {CustomView} from '../../components/ui/CustomView';
+import {Title} from '../../components/ui/Title';
+import {Button} from '../../components/ui/Button';
 
 export const ModalScreen = () => {
-  const [isVisible, setIsVisible] = useState(false)
-// render
+  const [isVisible, setIsVisible] = useState(false);
+  // render
   return (
     <CustomView margin>
-      <Title title="Modal" safe  />
-      <Button text="Open Modal"  onPress={()=>setIsVisible(true)} />
+      <Title title="Modal" safe />
+      <Button text="Open Modal" onPress={() => setIsVisible(true)} />
       <Modal visible={isVisible} animationType="slide">
-        <Title title="Modal Content" safe />
+        <View style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.1)'}}>
+          <View
+            style={{
+              backgroundColor: 'white',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Title title="Modal Content" />
+            <Button text="Close" onPress={() => setIsVisible(false)} />
+          </View>
+        </View>
       </Modal>
     </CustomView>
-  )
-}
+  );
+};
